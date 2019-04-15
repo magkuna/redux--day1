d9e8c0d87d33a8fad15053d9d2875b8baa843c76
magkuna <magkuna@gmail.com>
2019-04-15 11:08:23 +0200

    firs action in state changing

--- src/store.js
+++ src/store.js
@@ -1,5 +1,29 @@
-import { createStore} from 'redux'
+import { createStore } from 'redux'
 
-const reducer = () => {}
+const initialState = {
+    number: 0
+}
 
-export const store = createStore(reducer)
+const reducer = (state = initialState, action) => {
+    console.log(state, action)
+    
+    if(action.type === 'INC'){
+       return {
+           ...state,
+           number: state.number + 1
+       } 
+    } 
+        return state
+    }
+}
+
+    export const store = createStore(
+        reducer,
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+    const incAction = {
+        type: 'INC'
+
+    }
+
+    store.dispatch(incAction)
