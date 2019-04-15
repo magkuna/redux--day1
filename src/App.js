a122b8fba95ab618190c7b8b3aa92016c40052f1
magkuna <magkuna@gmail.com>
2019-04-15 14:21:08 +0200

    wpisywanie wiadomości po funkcji .map

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
 import React from 'react';
 
-import { sendMessage } from './store'
+import './store'
+import { sendActionCreator } from './state/messages'
 
 class App extends React.Component {
 
+    state = {
+        messages: []
+    }
 
+    prepareMessage = () => {
+        const receiver = prompt('Wpisz odbiorę')
+        const text = prompt('Wpisz tekst wiadomości')
 
+        const message = sendActionCreator(receiver, text)
+
+        this.setState({
+            messages: this.state.messages.concat(message)
+        })
+    }
     render() {
         return (
             <div>
                 <button
-                    onClick={() => sendMessage(
-                        prompt('Wpisz odbiorcę'),
-                        prompt('Wpisz wiadomość')
-                    ) }
+                    onClick={this.prepareMessage}
                 >
+                    Prepare Message
+            </button>
+                <button
+                    onClick={this.sendMessage}>
                     Send Message
             </button>
+                <div>
+                    {
+                        this.state.messages.map(
+                            (message, i) => (
+                                <div
+                                    key={i + message.text}
+                                >
+                                    {message.receiver} | {message.text}
+                                </div>
+                            )
+                        )
+                    }
+                </div>
             </div>
         )
     }
 }
-export default App;
+
+export default App
