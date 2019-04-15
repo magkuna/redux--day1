1cfc1641b25cc65127b268f91231fa7fe2e94464
magkuna <magkuna@gmail.com>
2019-04-15 11:49:43 +0200

    counter file added and exported to store.js creating INC DEC

---
+++ src/state/counter.js
@@ -0,0 +1,34 @@
+const INC = 'counter/INC'
+const DEC = 'counter/DEC'
+
+export const incActionCreator = () => ({
+    type: INC
+
+})
+export const decActionCreator = () => ({
+    type: DEC,
+
+})
+
+const initialState = {
+    number: 0
+}
+
+export const reducer = (state = initialState, action) => {
+    console.log(state, action)
+
+    if (action.type === INC) {
+        return {
+            ...state,
+            number: state.number + 1
+        }
+    }
+    if (action.type === DEC) {
+        return {
+            ...state,
+            number: state.number - 1
+        }
+    }
+    return state
+
+}
--- src/store.js
+++ src/store.js
@@ -1,41 +1,13 @@
 import { createStore } from 'redux'
 
-const initialState = {
-    number: 0
-}
-
-const reducer = (state = initialState, action) => {
-    console.log(state, action)
-
-    if (action.type === 'INC') {
-        return {
-            ...state,
-            number: state.number + 1
-        }
-    }
-    if (action.type === 'DEC') {
-        return {
-            ...state,
-            number: state.number - 1
-        }
-    }
-    return state
-
-}
+import {reducer,incActionCreator, decActionCreator} from './state/counter'
 
 
 export const store = createStore(
     reducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
-const incAction = {
-    type: 'INC',
 
-}
-const decAction = {
-    type: 'DEC',
 
-}
-
-store.dispatch(incAction)
-store.dispatch(decAction)
+store.dispatch(incActionCreator())
+store.dispatch(decActionCreator())
